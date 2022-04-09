599d875ae6a2795268d4e80a37a01fe1c02081ee
Sam Guymer <sam@guymer.me>
2022-04-09 17:28:42 +1000

    UI tweaks

--- startx.sh
+++ startx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-readonly scale="${1:-1}"
+readonly scale="${1:-1.5}"
 readonly xrandr_scale=$(awk -v v="${scale}" 'BEGIN { printf "%.2f", 2 / v; exit(0) }')
 readonly gtk_scale=$(awk -v v="${scale}" 'BEGIN { printf "%.2f", v / 2; exit(0) }')
 readonly dpi=$(awk -v v="${scale}" 'BEGIN { printf "%.0f", 96 * v; exit(0) }')
